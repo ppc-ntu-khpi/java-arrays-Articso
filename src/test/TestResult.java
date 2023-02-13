7c0e1081a41970aa167cc35754f50f8697ddbec4
Articso <101022731+Articso@users.noreply.github.com>
2023-02-13 17:12:39 +0200

    Add files via upload

---
+++ src/test/TestResult.java
@@ -0,0 +1,10 @@
+package test;
+
+import domain.Exercise;
+
+public class TestResult {
+    public static void main(String[] args) {
+        Exercise exercise = new Exercise();
+        exercise.display();
+    }
+}
